36367ded329e06b53ef14b2cd2bada471b95ce23
VuongND9 <nguyendinhvuong.act@gmail.com>
2022-11-19 23:35:03 +0700

    update TODO part

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,6 +27,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage/", async (req,res) => {
+    let image_url = req.query;
+
+    if(!image_url){
+      return res.status(400).send({message:"url is required"});
+    }
+
+    try{
+      const filteredPath = await filterImageFromURL(image_url);
+      res.sendFile(filteredPath, () => {
+        deleteLocalFiles([filteredPath])
+      })
+    }catch(err){
+      res.status(401).send({message:"url is invalid"})
+    }
+  })
 
   //! END @TODO1
   
